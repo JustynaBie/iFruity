f44422c1c7756e39e2e76742e174f900fe567dc7
Justyna Bielecka <justynbielecka@gmail.com>
2016-04-18 17:02:53 +0200

    seccion mix and moving ingredients

--- js/app.js
+++ js/app.js
@@ -1,7 +1,9 @@
 $(function(){
 
+  var hamburger = $("#hamburger");
+
   function checkSize(){
-   var maxWidth = window.matchMedia("(max-width: 650px)");
+   var maxWidth = window.matchMedia("(max-width: 600px)");
    maxWidth.addListener(WidthChange);
    WidthChange(maxWidth);
 
@@ -17,8 +19,101 @@ $(function(){
  }
 checkSize();
 
-  $("#hamburger").on("click", function(){
-    $("nav").slideToggle();
+  hamburger.on("click", function(){
+    var menu = $("nav");
+    menu.toggleClass("show-menu");
+    menu.find("li").toggleClass("show-li");
+    if(menu.hasClass("show-menu")){
+        menu.toggle(300);
+    }else{
+      menu.toggle();
+    }
   });
 
+  //function to clear position, and remove class from item when it is adden to the new site in DOM
+  function clearItem(item){
+    item.css("position", "");
+    item.css("top", "");
+    item.css("left", "");
+    item.removeClass("fruit-around");
+  };
+
+function moveIngredients(item){
+
+  var containerFruits = $(".container-fruits-items");
+  var containerBase = $(".container-base-items");
+  var containerMix = $(".container-glass-items");
+
+  var containerMixTop = containerMix.offset().top;
+  var containerMixLeft = containerMix.offset().left;
+
+  var containerFruitsLeft = containerFruits.offset().left;
+  var containerFruitsTop = containerFruits.offset().top;
+  var containerBaseLeft = containerBase.offset().left;
+  var containerBaseTop = containerBase.offset().top;
+
+  var itemTop = item.offset().top;
+  var itemLeft = item.offset().left;
+  var itemWidth = item.css("width");
+  console.log(itemWidth);
+
+  item.css("top", itemTop + "px");
+  item.css("left", itemLeft + "px");
+  item.css("position", "fixed");
+  item.css("z-index", "10");
+  item.addClass("fruit-around");
+
+  if(item.parent().hasClass("container-fruits-items") || item.parent().hasClass("container-base-items")){
+    item.animate({
+        left:  containerMixLeft + "px",
+        top: containerMixTop  + "px",
+    }, 800, function(){
+      var newSite = item.detach();
+      containerMix.prepend(newSite);
+      newSite.css("margin-left", "0.1rem");
+      newSite.css("margin-right", "0.1rem");
+      clearItem(newSite);
+
+    });
+  }else if(item.hasClass("fruit")){
+    item.animate({
+      left: containerFruitsLeft + "px",
+      top: containerFruitsTop + 2*(parseInt(itemWidth)) + "px",
+    }, 800, function(){
+      var newSite = item.detach();
+      containerFruits.append(newSite);
+      clearItem(newSite);
+    });
+  }else{
+    item.animate({
+      left: containerBaseLeft + 2*(parseInt(itemWidth)) + "px",
+      top: containerBaseTop  + "px",
+    },800, function(){
+      var newSite = item.detach();
+      containerBase.append(newSite);
+      clearItem(newSite);
+    });
+  }
+
+};
+
+
+var fruits =   $(".container-fruits-items").find(".item");
+
+fruits.on("click", function(){
+  if(($(".container-fruits-items").find(".item").length > 6) || ($(this).parent().hasClass("container-glass-items"))){
+      moveIngredients($(this));
+  }
+});
+
+var base = $(".container-base-items").find(".item");
+
+base.on("click", function(){
+  if(($(".container-base-items").find(".item").length > 2) || ($(this).parent().hasClass("container-glass-items"))){
+      moveIngredients($(this));
+  }
+});
+//
+//  $(".container-fruits-items").find(".item").draggable();
+//
 });
