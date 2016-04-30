53c34717c469d5bf9cb0deca5d06f97840592c3c
Justyna Bielecka <justynbielecka@gmail.com>
2016-04-30 17:08:32 +0200

    php file

---
+++ php/js.php
@@ -0,0 +1,18 @@
+<?PHP
+
+  $file = file_get_contents(__DIR__.'/../js/ifruity.json');
+  $fruits = json_decode($file);
+  // var_dump($fruits);
+  $id = (int)$_GET['id'];
+  $type = $_GET['type'];
+  $fruit = getFruitById($fruits->$type, $id);
+  echo json_encode($fruit);
+
+  function getFruitById($fruits, $id){
+     foreach($fruits as $fruit) {
+       if($fruit->id === $id) {
+         return $fruit;
+       }
+     }
+  }
+?>
